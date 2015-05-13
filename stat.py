e74811176d67c70a1ba7a15cdc550997aa12d86d
HulkBulk <michou_ath@hotmail.fr>
2015-05-13 08:58:41 -0700

    Add Force Graph representation

--- stat.py
+++ stat.py
@@ -1,12 +1,9 @@
 import pandas as pd
 import numpy as np
+import json
 import matplotlib.pyplot as plt
-from pandas.tools.plotting import scatter_matrix
 plt.show()
-
-
-
-def splitDataPerMatch(file='PremierLeague2011-12MatchbyMatch.csv')
+def splitDataPerMatch(file='PremierLeague2011-12MatchbyMatch.csv'):
 	"""Split all data per match and date
 		@params: file path to analys football stats
 		-------
@@ -15,27 +12,36 @@ def splitDataPerMatch(file='PremierLeague2011-12MatchbyMatch.csv')
 	"""
 	# Load Premier League 2011/2012 data
 	df = pd.read_csv(file,sep=';')
+
 	# Useless columns for first analysis
 	columnNotUsed = ['Player ID','Team Id']
+
 	# Drop all useless data
 	df = df.drop(columnNotUsed,axis=1)
+
 	# Group by match (You see only two team per 'Opposition Id')
 	op= df.groupby(['Team','Opposition','Date'])
-	#op = df.groupby('Date')
+
 	group = dict(list(op))
-	#small = pd.DataFrame(df[['Shots On Target inc goals','Goals','Right Foot Shots On Target']])
-	#scatter_matrix(small, alpha=0.2, figsize=(6, 6), diagonal='kde')
-	plt.show()
+
+	# Match statistics per date
 	match_stat = {}
+	
 	for key in group:
 		df_tmp = pd.DataFrame(group[key])
-		if key not in match_stat:
-			match_stat[key] = {}
+		key1 = str(key).replace(',',' ').replace(')',' ').replace('(',' ').replace("'",' ').strip()
+		if key1 not in match_stat:
+			match_stat[key1] = {}
 		tmp_match_dict = {}
+		
+		#All player into the current match (See key)
 		all_players = {}
 		currentposPlayer = {}
+
+		# Fullname definition
 		forename = list(group[key]['Player Forename'])
 		surname  = list(group[key]['Player Surname']) 
+
 		for i in xrange(len(forename)):
 			fullname = str(forename[i])+' '+str(surname[i])
 			all_players[fullname] = {}
@@ -44,5 +50,76 @@ def splitDataPerMatch(file='PremierLeague2011-12MatchbyMatch.csv')
 			grou = list(group[key][key_group])
 			for i in xrange(len(grou)):
 				all_players[currentposPlayer[i]][key_group] = grou[i]
-		match_stat[key] = all_players
+		match_stat[key1] = all_players
 	return match_stat
+def forceGraph(match_stat):
+	allnodes = {'nodes':[]}
+	links = {'links':[]}
+	primaryNodes = []
+	nodesHash = {}
+	linksArray = []
+	nodesArray = []
+	#structure { "nodes": [ {"name":"p1"}
+	counter = 0
+	for key in match_stat:
+		node = { 'label' : key, 'id' : counter, 'color' : "green", 'textcolor' : "black", 'size' : 20, 'desc' : "" }
+		if node['label'] not in nodesHash:
+			allnodes['nodes'].append(node)
+			nodesArray.append(node)
+			nodesHash[node['label']] = counter
+			counter += 1
+		for player in match_stat[key]:
+			stats = match_stat[key][player]
+			nodeplayer = { 'label' : player, 'id' : counter, 'color' : "red", 'textcolor' : "black", 'size' : 5, 'desc' : "" }
+			if nodeplayer['label']  not in nodesHash:
+				allnodes['nodes'].append(nodeplayer)
+				nodesArray.append(nodeplayer)
+				nodesHash[nodeplayer['label']] = counter
+				counter += 1
+			participation = 0.0
+			stats_weight = {}
+			#json.dumps(stats_weight,'weigthedmeta.json')
+			#for key in stats.keys():
+			#	print key
+			#	stats_weight[key] = raw_input()
+			#json.dumps(stats_weight,'weigthedmeta.json')
+			#raw_input()
+			denom = 0.0
+			for keystat in stats:
+				try:
+					if 'unsucess' in keystat.lower():
+						participation += float(stats[keystat])*(-1)
+						denom += -1
+					elif 'sucessful' in keystat.lower():
+						participation += float(stats[keystat])*1
+						denom += 1
+					else:
+						participation += float(stats[keystat])
+						denom += 1
+				except:
+					print 'No float in to ',keystat
+					
+			participation = np.log(participation/denom+1)
+			if participation >0:
+				link = { 'desc' : "RAS", 'source' : nodesHash[nodeplayer['label']], 'target' : nodesHash[node['label']], 'weight' : participation, 'color' : "grey" } 
+				linksArray.append(link)
+			#links['links'].append({'source':player,'target':key,'weight':participation})
+	return nodesHash,linksArray,nodesArray
+def writefile(nodesHash,linksArray,nodesArray):
+	with open('Levenshtein.js','wb') as f:
+		f.write('var nodesArray = [\n')
+		for node in nodesArray[:-1]:
+			f.write(str(node)+',\n')
+		f.write(str(nodesArray[-1])+'\n')
+		f.write('];\n')
+		f.write('var nodesHash = [];')
+		for key in nodesHash:
+			f.write('nodesHash["'+str(key)+'"]='+str(nodesHash[key])+';\n')
+		f.write('var linksArray = [\n')
+		for links in linksArray[:-1]:
+			f.write(str(links)+',\n')
+		f.write(str(linksArray[-1])+'\n')
+		f.write('];')
+match_stat = splitDataPerMatch()
+nodesHash,linksArray,nodesArray = forceGraph(match_stat)
+writefile(nodesHash,linksArray,nodesArray)
